af919d26770674ee0d1ab70f8e50f38dbbff4162
MiguelRen <migueleduardorengel@gmail.com>
2023-11-20 23:38:35 -0400

    I think yes now

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"/repoprueba4.github.io/",
   plugins: [vue()],
-  base:"/repoprueba4.github.io/"
 })
